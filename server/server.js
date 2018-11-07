e4659b7c64f369c634e28fc8d28796d1c6ea9f5f
Ravikant Yadav <ravikant@Ravikants-MacBook-Air.local>
2018-11-07 16:25:56 +0530

    Port fix

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var express = require('express');
 var publicPath = path.join(__dirname ,'../public');
-var port = process.env.port || 3000
+var port = process.env.PORT || 3000
 var app = express();
 app.use(express.static(publicPath));
 
