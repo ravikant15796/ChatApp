5533fcbcf4ffc47f14e955f9301640f4eb6e73c7
Ravikant Yadav <ravikant@Ravikants-MacBook-Air.local>
2018-11-11 15:12:32 +0530

    adding timestamps

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+
+
 var socket =  io();
        socket.on('connect' , function(){
            console.log('Connected to server');
@@ -6,15 +8,16 @@ var socket =  io();
            console.log('Disconnected from server');
        })
        socket.on('newMsg',function(msg){
-           console.log('newMsg', msg)
+           var formattedTime = moment(msg.createdAt).format('h:mm a');
            var li = jQuery('<li></li>');
-           li.text(`${msg.from}: ${msg.text}`);
+           li.text(`${msg.from} ${formattedTime}: ${msg.text}`);
            jQuery('#messages').append(li);
        });
        socket.on('newMsgLocation', function(message){
+        var formattedTime = moment(message.createdAt).format('h:mm a');
           var li = jQuery('<li></li>');
           var a = jQuery('<a target="_blank">My current location</a>');
-          li.text(`${message.from}: `);
+          li.text(`${message.from} ${formattedTime} : `);
           a.attr('href',message.url);
           li.append(a);
           jQuery('#messages').append(li);
@@ -25,28 +28,32 @@ var socket =  io();
        },function(data){
            console.log('Got it', data)
        });
-
+     
+       var msgText = jQuery('[name=message]');
        jQuery('#message-forms').on('submit' , function(e){
            e.preventDefault();
 
           socket.emit('createMsg',{
             from :'user',
-            text : jQuery('[name=message]').val()
+            text : msgText.val()
           },function(){
-
+           msgText.val('');
           }); 
        });
        var locationButton = jQuery('#send-location');
        locationButton.on('click', function(){
         if(!navigator.geolocation){
-            alert('Geolocation is not supproted by your browser');
+            return alert('Geolocation is not supproted by your browser');
         }
+        locationButton.attr('disabled','disabled').text('Sending Location..');
         navigator.geolocation.getCurrentPosition(function(position){
+            locationButton.removeAttr('disabled').text('Send Location');
             socket.emit('getLocation',{
                 latitude : position.coords.latitude,
                 longitude : position.coords.longitude
             });
        },function(){
+           locationButton.removeAttr('disabled').text('Send Location');
             alert('Unable to fetch location');
         });
        });
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,15 +1,16 @@
+var moment = require('moment');
 var generateMessage = (from,text)=>{
 return {
     from,
     text,
-    createdAt : new Date().getTime()
+    createdAt : moment.valueOf()
 };
 };
 var generateNewMessageLocation = (from,latitude , longitude)=>{
     return {
         from ,
         url : "https://www.google.co.in/maps?q="+ latitude +"," +longitude,
-        createdAt : new Date().getTime()
+        createdAt :  moment.valueOf()
 
     }
 }
